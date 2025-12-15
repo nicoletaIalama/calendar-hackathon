7b08c2d5388d8ddae30e02d7f2f0e6a533cddbe9
Chris Flynn <chris.flynn@enable.com>
2025-12-15 15:23:38 +0000

    Add NoKey for DashboardHoliday entity

--- Calendar/Domain/DashboardModels/DashboardDbContext.cs
+++ Calendar/Domain/DashboardModels/DashboardDbContext.cs
@@ -8,5 +8,13 @@ namespace Calendar.Domain.DashboardModels
         public DashboardDbContext(DbContextOptions<DashboardDbContext> options) : base(options) { }
         public DbSet<User> Users { get; set; } = null!;
         public DbSet<DashboardHoliday> Holidays { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure DashboardHoliday as keyless
+            modelBuilder.Entity<DashboardHoliday>().HasNoKey();
+        }
     }
 }
