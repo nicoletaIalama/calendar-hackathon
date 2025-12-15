52714dc9e49309942521f2b79b89dd17e4bc3dac
Chris Flynn <chris.flynn@enable.com>
2025-12-15 14:32:52 +0000

    Add model for Dashboard holidays

---
+++ Calendar/Domain/DashboardModels/DashboardHoliday.cs
@@ -0,0 +1,9 @@
+﻿namespace Calendar.Api.Domain.DashboardModels;
+
+public class DashboardHoliday
+{
+    public string Initials { get; set; } = default!;
+    public DateOnly HolidayDate { get; set; }
+    public int HolidayType { get; set; }
+    public decimal HolidaySize { get; set; }
+}
