b18a0aa5b547ebb03f22bb5fbc33fafbcfee300e
Chris Flynn <chris.flynn@enable.com>
2025-12-15 11:31:52 +0000

    Add Swagger

--- Calendar/Program.cs
+++ Calendar/Program.cs
@@ -8,6 +8,9 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +19,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+} else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseRouting();
