6923ec6b52e3ef1affb434255f752b6a6ac954f9
Nicoleta Ialama <Nicoleta.Ialama@enable.com>
2025-12-15 10:49:34 +0000

    razor

---
+++ Calendar/Domain/Entities/CalendarView.cs
@@ -0,0 +1,12 @@
+﻿namespace Calendar.Domain.Entities;
+
+public class CalendarView
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public List<Guid> EmployeeIds { get; set; } = new();
+
+    public Guid? OwnerUserId { get; set; }
+}
