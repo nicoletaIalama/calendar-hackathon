38a14f94333174421c86f1b44c2e50882e9bdda6
Nicoleta Ialama <Nicoleta.Ialama@enable.com>
2025-12-15 11:01:23 +0000

    views and calendar page

--- Calendar/Domain/Entities/Team.cs
+++ Calendar/Domain/Entities/Team.cs
@@ -4,5 +4,6 @@ public class Team
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = null!;
+
     public ICollection<Employee> Members { get; set; } = new List<Employee>();
 }
