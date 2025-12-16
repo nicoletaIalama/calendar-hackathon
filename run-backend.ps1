42aa1982743e86097a0da7b6f519089622b708ea
Chris Flynn <chris.flynn@enable.com>
2025-12-16 13:41:37 +0000

    Add watch to startup commands

--- run-backend.ps1
+++ run-backend.ps1
@@ -11,7 +11,7 @@ Write-Host "Starting backend (Calendar) in background..."
 
 $proc = Start-Process `
   -FilePath "dotnet" `
-  -ArgumentList @("run", "--project", $project, "--launch-profile", $LaunchProfile) `
+  -ArgumentList @("watch", "run", "--project", $project, "--launch-profile", $LaunchProfile) `
   -WorkingDirectory $root `
   -PassThru `
   -WindowStyle Minimized
